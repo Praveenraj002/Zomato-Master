f5e183b2cbcbf6be0a1675b065f2c983aefa4f55
Praveenraj002 <praveenbose264@gmail.com>
2021-08-26 19:54:14 +0530

    backed up codes

---
+++ server/index.js
@@ -0,0 +1,42 @@
+// Importing Env Variables
+require("dotenv").config();
+
+//Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+
+//configs
+import googleAuthConfig from "./config/google.config";
+
+//microservices Routes
+import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+
+//database Connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+//application middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended: false }));
+zomato.use(helmet());
+zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+//passport config
+googleAuthConfig(passport);
+
+//Application Routes
+zomato.use("/auth", Auth);
+zomato.use("/restaurant", Restaurant);
+
+zomato.get("/", (req, res) => res.json({ message: "setup success"}));
+
+zomato.listen(4000, () => 
+ConnectDB()
+.then(() => console.log("Server is Running"))
+.catch(() => console.log("Server is Running, but Database Connection Failed")) );
