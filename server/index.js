a31a6a768997afce019fa51ddefc715e2e8ec655
Praveenraj002 <praveenbose264@gmail.com>
2021-08-28 18:09:44 +0530

    adding menu api

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ import googleAuthConfig from "./config/google.config";
 //microservices Routes
 import Auth from "./API/Auth";
 import Restaurant from "./API/Restaurant";
+import Food from "./API/Food"
+import Menu from "./API/menu";
 
 //database Connection
 import ConnectDB from "./database/connection";
@@ -33,6 +35,8 @@ googleAuthConfig(passport);
 //Application Routes
 zomato.use("/auth", Auth);
 zomato.use("/restaurant", Restaurant);
+zomato.use("/food", Food);
+zomato.use("/menu", Menu);
 
 zomato.get("/", (req, res) => res.json({ message: "setup success"}));
 
